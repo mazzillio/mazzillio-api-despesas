a71de14ab86eb1070b577f9c27ef64c66f01d461
mazzillio <mateca500@gmail.com>
2025-05-27 23:18:31 -0300

    iniciando documentação

--- src/despesas/dto/create-despesa.dto.ts
+++ src/despesas/dto/create-despesa.dto.ts
@@ -6,18 +6,31 @@ import {
   IsString,
 } from 'class-validator';
 import { Categoria } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateDespesaDto {
   @IsNotEmpty({ message: 'O título é obrigatório' })
   @IsString()
+  @ApiProperty({
+    description: 'O título da despesa',
+    example: 'Compra de alimentos',
+  })
   title: string;
 
   @IsNotEmpty({ message: 'O valor é obrigatório' })
   @IsNumber()
+  @ApiProperty({
+    description: 'O valor da despesa',
+    example: 100.0,
+  })
   amount: number;
 
   @IsNotEmpty({ message: 'A categoria é obrigatória' })
   @IsEnum(Categoria)
+  @ApiProperty({
+    description: 'A categoria da despesa',
+    example: `${Categoria.ALIMENTACAO} | ${Categoria.SAUDE} | ${Categoria.TRANSPORTE} | ${Categoria.LAZER} | ${Categoria.OUTROS}`,
+  })
   categoria: Categoria;
 
   @IsNotEmpty({ message: 'A data é obrigatória' })
